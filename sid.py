3d0f588bb695e426733e6d252a2f40cf1f8b3f24
gbdu <ogrum@live.com>
2015-08-15 09:00:22 -0400

    etched out basic structure, does not wokr at this point...

--- sid.py
+++ sid.py
@@ -3,42 +3,9 @@
 __author__ = 'gargantua'
 
 
-class Sid:
-    ''' A form of basic intelligence, using using 64 Components :
-
-        receive:    listen to 5 audio streams,
-                    read 5 text streams,
-                    watch 5 video streams;
-
-        do:     talk about current state from audio snippets collected,
-                type about current state from internal text collected,
-                paint video art about using visual data collected;
-
-        to use, simply:
-            import sid
-            new_guy = sid.new()
-            new_guy.live("hi there") # new guy will now run indefinitely, taking user input
-                    # you can kill it by simply saying: die
-                        $> ned is now listening to you...die
-                         < ned dies>
-                        $ # the end
-
-
-        Remember: you are to human what ned is to sid
-            -> sid is a type of being
-                    ned is a sid
-                    you are a human
-
-                        #TODO: study these relations
-'''
-
 import threading
 
-import region
-
-# todo: main todo
-
-class sid:
+class Ned:
     myname = None
     mythreads = []
     shutdown_event = threading.Event()
@@ -46,32 +13,9 @@ class sid:
     def __init__(self):
         pass
 
-    def init_a_thing(self, of_type):
-        for i in range(21):
-            new_component = region.region(i, of_type)
-            t = threading.Thread(target=new_component, args=(i,))
-            t.daemon = True
-            t.start()
-            self.mythreads.append(t)
-
-    def build_components(self):
-        # build 21 audio components thing for audio medium
-        self.init_a_thing("audio")
-        # build 21 video components thing for video medium
-        self.init_a_thing("video")
-        # build 21 langu components thing for text medium
-        self.init_a_thing("langu")
-
-        x = region.region(64, "x")  # the last region.py
-        tx = threading.Thread(target=x, args=(64,))  # the last interesting region.py...
-        tx.daemon = True
-
-        self.mythreads.append(tx)  # add the x component to the mix...
-
-
     def live(self, name, number):
         self.myname = name
-        self.build_components()
+#        self.build_components()
 
     def count_human_components(self):
         return 3;
@@ -85,68 +29,16 @@ class sid:
     def died(self):
         pass
 
-    def print_pane(self):
-        sid_panel = "" \
-                    " #                             HELLO, FATHER, MY NAME IS sid.name " \
-                    " #             [x] sid is 3+1 things # implemented as one python class/implementation a thing is 21 component s   ; " \
-                    " #                 [-] a component is 9*1000 neurons, layered in various pre-defined patterns for each            ; " \
-                    " #                 [-] a neuron is an atomic unit, *smart* algro                                                  ; " \
-                    " #                     [ ] input: 3 types (I have 3 built in organ for those...                                 ; " \
-                    " #                     " \
-                    "                       " \
-                    "                       " \
-                    "                       IT DOES ONLY TWO THINGS: " \
-                    " #                             [ ]  from the onset, the neuron 'learns' (a value, intensity)                                                                  ; " \
-                    " #                                                        ; " \
-                    " #                and produces a signal (8channel intensity value...)                                             ; " \
-                    " #                     -REMEMBER:  ** its the connection between the neurons                                      ; " \
-                    " #                     -REMEMBER: **  that really matters for output!!!! , make sure its one one of the chans     ; " \
-                    " #                 -> thats it for now boy                                                                        ; " \
-                    " #        intensity_channel_1 is measured by how many other neurons connect to it!'; # thats it                   ; ";
+    def print_panel(self):
+        sid_panel = ""
         return sid_panel
 
     def name(self):
         return self.myname;
 
 
-ned = sid()
+ned = Ned()
 ned.live("ned", 45)
-
-# """
-# print a panel describing ned's state.... this is a static func containing 65 threads...
-#   :return:
-# """
-def print_panel():
-    panel = ("""
-                    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --                          
-                                   Hello, FATHER
-            Name: """ + ned.name()  + """
- I have 3 organs, with 21 components each. Right now, I am watching
- + ned.get_media_numbers() + things. On ( ned.get_active_components )  different  computers
- If you want, tell me to watch more media, I can do 21 sources for                             
- each type (23 audio, 23 video, 23 language) . Here are the commands                           
- for each media you wish to assign to each of my things:                                       
-                                                                                               
-                                                                                               
-       - watch url_to_yt      # dedicate 2 components to watching youtube vids                
-                                                                                              
-       - watch me             # Will dedicate 12 components to webcam                         
-                                                                                               
-                                                                                               
-;                 #   watch file/url_to_mp3_stream
-;                 #   I have ran everything " + str(times_ran) + " times in the last "  ; # and this is the last line
-
-        """
-    )
-
-    panel = "Name   : %s, I have %d organs and %d components, each with %d neuron (total %d neurons)" \
-            "I am watching %d different data streams. Tell me to watch more stuff by saying:" \
-            "   - watch_video url_to_yt";
-
-
-
-    print panel
-    return panel
 
 
 while True:
@@ -154,8 +46,7 @@ while True:
         # this thread hangs on user input, therefore we cannot print
         # an accurate representation of ned here... Use a thread instead...
 
-
-        t_panel = threading.Thread(target=print_panel)
+        t_panel = threading.Thread(target=ned.print_panel)
         # the last interesting region.py...
         t_panel.daemon = True
         t_panel.start() # start printing output
