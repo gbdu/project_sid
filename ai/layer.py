3680f0a455c7f588187980f34d8cfb0022c43746
gbdu <ogrum@live.com>
2015-08-25 13:51:06 -0400

    A

---
+++ ai/layer.py
@@ -0,0 +1,20 @@
+"""
+
+A very basic layer of neurons, a component has about 8 layers
+
+
+"""
+
+__author__ = 'gbdu'
+__status__ = "dev"
+
+
+from neuron import Neuron
+
+class Layer():
+	neurons = None
+
+	def __init__(self, num_of_neurons=1024):
+		for i in num_of_neurons:
+			new_bit = Neuron(molecule_pool, l)
+		pass
